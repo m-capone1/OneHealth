faf4098132e35659b1bdff3a22b985aeb1d8b475
srwa-rsp <srwa.rashidpoor.1993@gmail.com>
2024-08-09 07:31:53 -0400

    add header to home page

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Header from "../../components/Header/Header";
 
 const HomePage = () => {
   return (
-    <div className="homepage">
+    <> 
+      <Header />
     {/* <p>Landing Page</p> placeholder */}
-      <Link to="/chat" className="homepage__link">
+      {/* <Link to="/chat" className="homepage__link">
         Go to Chat
-      </Link>
-    </div>
+      </Link> */}
+    </>
   );
 };
 
