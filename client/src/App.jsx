61396e654f3f0ea08bedffc3ea486913d41661c7
Maddy Capone <maddycapone@gmail.com>
2024-08-09 11:45:44 -0300

    add route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/chat" element={<ChatPage />} />
         <Route path='/records' element={ <RecordList recordListData={recordData}/>} />
+        <Route path='/record-details' element={ <RecordDetailsPage  />} />
       </Routes>
     </Router>
   );
