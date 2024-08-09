784102141fd524728a82ddf21c162942df59e79f
Maddy Capone <maddycapone@gmail.com>
2024-08-08 23:59:47 -0300

    more styling and call server for open ai content

--- client/src/pages/RecordDetailsPage/RecordDetailsPage.jsx
+++ client/src/pages/RecordDetailsPage/RecordDetailsPage.jsx
@@ -8,17 +8,21 @@ export default function RecordDetailsPage() {
     const [showDetails, setShowDetails] = useState(false);
     const [content, setContent]= useState({});
 
-    const openAIUrl = "";
-    const date = '2024/08/01';
-    let prompt = "Explain what "
+    const openAIUrl = "http://localhost:8080/generate-content";
+    let prompt = "Explain what the different blood types are."
 
     useEffect(()=> {
         const fetchContent = async() => {
-            const response = axios.post(openAIUrl, {
-                prompt: {}
-            })
-        }
+            const response = await axios.post(openAIUrl, {
+                prompt: prompt
+            });
 
+            if(response){
+                setContent(response.data);
+                console.log(response.data);
+            }
+        }
+        fetchContent();
     }, [])
 
     const handleDetails = () => {
@@ -46,10 +50,11 @@ export default function RecordDetailsPage() {
             <div className='details__content-wrapper'>
                 {showDetails ? (
                     <div className='details__content'>
-                        <div>Test Name: {date}</div>
-                        <div>Collection Date: {date}</div>
-                        <div>Test Results: {date}</div>
-                        <div>Lab Name: {date}</div>
+                        <div>Patient id: 0956372</div>
+                        <div>Test Name: Full Panel Bloodwork</div>
+                        <div>Collection Date: 2024/08/01</div>
+                        <div>Doctor's Name: Dr. Jane Doe</div>
+                        <div>Lab Name: Life Labs</div>
                     </div>
                 ) : (
                     <div className='details__empty'></div>
@@ -57,7 +62,8 @@ export default function RecordDetailsPage() {
             </div>
             <div className='details__layout'>
                 <img 
-                    src={bloodwork} 
+                    src={bloodwork}
+                    onClick={handleGenerate} 
                     className='details__img' 
                     alt="bloodwork"
                 />
