8d22929467f8e55c3779e29d06d7e31c879d8c5c
Maddy Capone <maddycapone@gmail.com>
2024-08-08 23:16:55 -0300

    add some styling and bloodwork image, and also add details click event

--- client/src/pages/RecordDetailsPage/RecordDetailsPage.jsx
+++ client/src/pages/RecordDetailsPage/RecordDetailsPage.jsx
@@ -1,38 +1,67 @@
 import './RecordDetailsPage.scss';
-import { useState } from 'react';
-import bloodwork from '../../assets/images/bloodwork.png';
-import arrow from '../../assets/icons/down-arrow.png'
-export default function () {
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import bloodwork from '../../assets/images/bloodwork3.jpg';
+import arrow from '../../assets/icons/down-arrow.png';
 
-    const [showDetails, setShowDetails]= useState(false);
+export default function RecordDetailsPage() {
+    const [showDetails, setShowDetails] = useState(false);
+    const [content, setContent]= useState({});
+
+    const openAIUrl = "";
+    const date = '2024/08/01';
+    let prompt = "Explain what "
+
+    useEffect(()=> {
+        const fetchContent = async() => {
+            const response = axios.post(openAIUrl, {
+                prompt: {}
+            })
+        }
+
+    }, [])
 
     const handleDetails = () => {
         setShowDetails(!showDetails);
+    };
+
+    const handleGenerate = () => {
+
     }
 
     return (
         <section className='details'>
             <div className='details__heading'>
                 <h1 className='details__headers details__title'>Record: Full Panel Bloodwork</h1>
-                <div className='details__more'>
-                    <div>More details</div>
+                <div className='details__dropdown'>
+                    <div className='details__more'>More details</div>
                     <img 
                         onClick={handleDetails} 
-                        className= "details__arrow" 
-                        src={arrow} alt="arrow"
-                    ></img>
+                        className="details__arrow" 
+                        src={arrow} 
+                        alt="arrow"
+                    />
                 </div>
             </div>
-            {showDetails && (
+            <div className='details__content-wrapper'>
+                {showDetails ? (
                     <div className='details__content'>
-                        <p>Details about the bloodwork go here...</p>
+                        <div>Test Name: {date}</div>
+                        <div>Collection Date: {date}</div>
+                        <div>Test Results: {date}</div>
+                        <div>Lab Name: {date}</div>
                     </div>
+                ) : (
+                    <div className='details__empty'></div>
                 )}
-            <img 
-                src={bloodwork} 
-                className='details__img' 
-                alt="bloodwork">
-            </img>
+            </div>
+            <div className='details__layout'>
+                <img 
+                    src={bloodwork} 
+                    className='details__img' 
+                    alt="bloodwork"
+                />
+            </div>
         </section>
-    )
+    );
 }
