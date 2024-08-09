1fb8bc2e5e4bb451f872ea5f791d5578ecd3a754
kikiheda <qiqihe.da@gmail.com>
2024-08-08 21:48:43 -0400

    initalize chatbot page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,17 @@
-import './App.scss';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ChatPage from "./pages/ChatPage/ChatPage.jsx";
+import HomePage from "./pages/HomePage/HomePage.jsx";
+
 function App() {
-
   return (
-    <>
-
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/chat" element={<ChatPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
